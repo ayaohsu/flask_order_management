a1ec17618231eda108569a7406ce3f56baa37664
Tsung-Yao Hsu <ayao780607@gmail.com>
2024-01-15 08:33:34 +0800

    Test login required

--- app/app.py
+++ app/app.py
@@ -1,19 +1,34 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, login_required
+import secrets
 
 from user import User
 
 app = Flask("OrderNamagement")
+app.secret_key = secrets.token_hex()
 login_manager = LoginManager()
 
 @app.route('/')
 def hello():
 	return "Hello World!"
 
+@app.route('/login', methods=['POST'])
+def login():
+    user = User("1", "admin", "apass")
+    login_user(user)
+    app.logger.info('User logs in successfully')
+    return "Success", 201
+
+@app.route('/place_order', methods=['POST'])
+@login_required
+def place_order():
+    app.logger.info('Placing order')
+    return "Success", 201
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User()
+    return User().get_user_by_id(user_id) # TODO fix here
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
-	login_manager.init_app(app)
+    login_manager.init_app(app)
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- app/user.py
+++ app/user.py
@@ -1,6 +1,7 @@
 class User:
-    def __init__(self, id, password):
+    def __init__(self, id, username, password):
         self.id = id
+        self.username = username
         self.password = password
         self.is_authenticated = False
         self.is_active = True
@@ -8,3 +9,18 @@ class User:
     
     def get_id(self):
         return self.id
+    
+    def get_user_by_id(self, id):
+        if id == 1:
+            self.id = 1
+            self.username = 'admin'
+            self.password = 'apass'
+            return self
+        elif id == 2:
+            self.id = 2
+            self.username = 'customer'
+            self.password = 'bpass'
+            return self
+        else:
+            return None
+        
