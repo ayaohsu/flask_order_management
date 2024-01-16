c719ad8a67caac418d8b9e3c6439e95a258474e3
Tsung-Yao Hsu <ayao780607@gmail.com>
2024-01-16 23:37:51 +0800

    Use hash check to validate password

--- app/app.py
+++ app/app.py
@@ -1,42 +1,59 @@
-from flask import Flask
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask import Flask, request, Response
+from flask_login import LoginManager, login_user, logout_user, login_required, \
+    current_user
+from flask_bcrypt import Bcrypt
 
-from user import User, get_user_by_id
+from user import get_user_by_id, get_user_by_username
 
-app = Flask("OrderNamagement")
+
+app = Flask(__name__)
 app.secret_key = b'ae7566d003089e4107579c96077a20855827a3fa47f27733e0d1088afea8fd48'
-login_manager = LoginManager()
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+flask_bcrypt = Bcrypt(app)
+
 
 @app.route('/login', methods=['POST'])
 def login():
+    username = request.form['username']
+    password = request.form['password']
 
-    user = User(1)
-    login_user(user)
-    app.logger.info(f'User logs in successfully [user={user}]')
-    
-    return "Success", 201
+    user = get_user_by_username(username)
+    if user is None:
+        app.logger.info(f'Attempted to login with a non-existing username [username={username}]')
+        return Response('Username does not exist', status=400)
+
+    if flask_bcrypt.check_password_hash(user.password_hash, password):
+        login_user(user)
+        app.logger.info(f'Logged user in successfully [user={user}]')
+        return Response('Logged in', status=201)
+    else:
+         app.logger.info(f'Failed to log user in due to incorrect password [username={username}][password={password}]')
+         return Response('Login failed', status=400)
+
 
 @app.route('/logout', methods=['POST'])
 @login_required
 def logout():
+    user_id_to_logout = current_user.get_id()
     logout_user()
-    
-    return "Success", 201
+    app.logger.info(f'Logged out user successfully [user_id={user_id_to_logout}]')
+    return Response('Logged out', status=200)
+
 
 @app.route('/place_order', methods=['POST'])
 @login_required
 def place_order():
     app.logger.info('Placing order')
-    return "Success", 201
+    return Response('Success', status=200)
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return get_user_by_id(1)
+    return get_user_by_id(user_id)
+
 
 if __name__ == '__main__':
-    login_manager.init_app(app)
     app.run(host='0.0.0.0', port=8000, debug=True)
--- app/user.py
+++ app/user.py
@@ -2,13 +2,27 @@ from flask_login import UserMixin
 
 class User(UserMixin):
 
-    def __init__(self, id):
+    def __init__(self, id, username, password_hash):
         self.id = id
-        self.name = "name"
-        self.password = "password"
-        
+        self.username = username
+        self.password_hash = password_hash
+
     def __repr__(self):
-        return f"User[id={self.id}][name={self.name}]"
+        return f'User[id={self.id}][username={self.username}]'
+
+users = [
+    User('1', 'admin01', b'$2b$12$PVAArPGy0TlBfpULT5cShO6X4NToEqHU5r44ehPfut8NWQLBqrc0.'), # 'p@ss'
+    User('2', 'customer01', b'$2b$12$PQr/CiKJo8mt8uww4TxXmu1gP6gjPg61oGW81myWtJgVY5QgvKSMe') # 'w0rd'
+]
 
 def get_user_by_id(id):
-    return User(id)
+    for user in users:
+        if id == user.id:
+            return user
+    return None
+
+def get_user_by_username(username):
+    for user in users:
+        if username == user.username:
+            return user
+    return None
