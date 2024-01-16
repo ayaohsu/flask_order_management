f9a7a269281919c5ff7e8fc8a4eb91236a978f5d
Tsung-Yao Hsu <ayao780607@gmail.com>
2024-01-16 23:52:20 +0800

    Add postgreSQL instance

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, Response
-from flask_login import LoginManager, login_user, logout_user, login_required, \
-    current_user
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
 
 from user import get_user_by_id, get_user_by_username
