aeec24eb25cea208c305cf33cf43c93a52ee50ec
Tsung-Yao Hsu <ayao780607@gmail.com>
2024-01-20 17:44:21 +0800

    Update products

--- app/src/product.py
+++ app/src/product.py
@@ -21,6 +21,28 @@ def create_product():
         return Response("Failed to create product since it exists already.", 400)
 
 
+@product_app.route('/edit_product', methods=['POST'])
+@login_required
+def edit_product():
+    name = request.form['name']
+    price = float(request.form['price'])
+    stock = int(request.form['stock'])
+
+    current_app.logger.info(f"Received edit product request with [name={name}][price={price}][stock={stock}]")
+
+    product = get_product_by_name(name)
+    if product is None:
+        return Response("Product does not exist.", 400)
+    
+    product.price = price
+    product.stock = stock
+
+    if product.update_to_db():
+        return Response('Product updated.', 200)
+    else:
+        return Response('Failed to update product.', 500)
+
+
 class Product:
 
     def __init__(self, id, name, price, stock):
@@ -31,6 +53,32 @@ class Product:
 
     def __repr__(self):
         return f'Product[id={self.id}][name={self.name}][price={self.price}][stock={self.stock}]'
+    
+    def update_to_db(self):
+        connection = psycopg2.connect(**db_config)
+    
+        cursor = connection.cursor()
+        query_executed_successfully = True
+
+        try:
+            cursor.execute('''
+                UPDATE products
+                SET
+                price=%s,
+                stock=%s
+                WHERE id=%s
+            ''',
+            (self.price, self.stock, self.id)
+            )
+            connection.commit()
+        except Exception as e:
+            current_app.logger.error(f"Failed to update product to database. [exception={e}][product={self}]")
+            query_executed_successfully = False
+        finally:
+            cursor.close()
+            connection.close()
+        
+        return query_executed_successfully
 
 def insert_product_to_db(name, price, stock):
     connection = psycopg2.connect(**db_config)
@@ -56,3 +104,25 @@ def insert_product_to_db(name, price, stock):
         connection.close()
 
     return query_executed_successfully
+
+def get_product_by_name(name):
+    connection = psycopg2.connect(**db_config)
+    
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT id, name, price, stock 
+        FROM products
+        WHERE name = %s
+    ''',
+    (name,)
+    )
+
+    if cursor.rowcount == 0:
+        return None
+    
+    product_record = cursor.fetchone()
+    
+    cursor.close()
+    connection.close()
+
+    return Product(product_record[0], product_record[1], product_record[2], product_record[3])
