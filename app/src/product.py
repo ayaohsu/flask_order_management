1fe1d9293cdb771b4692171027357386e59582d4
Tsung-Yao Hsu <ayao780607@gmail.com>
2024-01-20 17:12:18 +0800

    create products

--- app/src/product.py
+++ app/src/product.py
@@ -1,14 +1,58 @@
 from flask import Blueprint, current_app, Response, request
 from flask_login import login_required
+import psycopg2
+
+from config import db_config
 
 product_app = Blueprint('product_app', __name__)
 
 @product_app.route('/create_product', methods=['POST'])
 @login_required
-def add_product():
+def create_product():
     name = request.form['name']
     price = request.form['price']
     stock = request.form['stock']
 
-    current_app.logger.info(f"create product with {name}/{price}/{stock}")
-    return Response("Product created.", 201)
+    current_app.logger.info(f"Received create product request with [name={name}][price={price}][stock={stock}]")
+    
+    if insert_product_to_db(name, price, stock):
+        return Response("Product created.", 201)
+    else:
+        return Response("Failed to create product since it exists already.", 400)
+
+
+class Product:
+
+    def __init__(self, id, name, price, stock):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+    def __repr__(self):
+        return f'Product[id={self.id}][name={self.name}][price={self.price}][stock={self.stock}]'
+
+def insert_product_to_db(name, price, stock):
+    connection = psycopg2.connect(**db_config)
+    
+    cursor = connection.cursor()
+    query_executed_successfully = True
+
+    try:
+        cursor.execute('''
+            INSERT INTO products
+            (name, price, stock)
+            VALUES 
+            (%s, %s, %s)
+        ''',
+        (name, price, stock)
+        )
+        connection.commit()
+    except psycopg2.errors.UniqueViolation:
+        current_app.logger.error(f"Attempt to create a product with an existing name. [name={name}]")
+        query_executed_successfully = False
+    finally:
+        cursor.close()
+        connection.close()
+
+    return query_executed_successfully
--- db/init_db.sql
+++ db/init_db.sql
@@ -1,5 +1,3 @@
-DROP TABLE users;
-
 CREATE TABLE users (
     id VARCHAR ( 10 ) PRIMARY KEY,
     username VARCHAR ( 20 ) NOT NULL,
@@ -7,10 +5,18 @@ CREATE TABLE users (
     role VARCHAR ( 15 ) NOT NULL
 );
 
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR ( 50 ) UNIQUE NOT NULL,
+    price NUMERIC NOT NULL,
+    stock INTEGER NOT NULL
+);
+
+
 INSERT INTO users
 (id, username, password_hash, role)
 VALUES
-('1', 'admin01', '$2b$12$PVAArPGy0TlBfpULT5cShO6X4NToEqHU5r44ehPfut8NWQLBqrc0.', 'admin');
+('1', 'manager01', '$2b$12$PVAArPGy0TlBfpULT5cShO6X4NToEqHU5r44ehPfut8NWQLBqrc0.', 'manager');
 
 INSERT INTO users 
 (id, username, password_hash, role)
